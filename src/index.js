009cf9d8d431f1d6cd6bdc8415e80e6e8c1a9932
Jaison DEV <jaison.alieve@gmail.com>
2020-06-09 11:46:41 -0300

    adicionados alguns dados de planetas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('ola cocos fedidos')
+const Planet = require("./planet")
+const solarSystem = require("./solar_system")
+const moment = require("moment")
+
+//importando arquivos de outros arquivos por //modulos!
+
+const earth = new Planet("Terra", 50100000)
+earth.rotate()
+
+solarSystem.planets.push(earth)
+solarSystem.planets.push(new Planet("marte", 50520005))
+solarSystem.planets.push(new Planet("plutão", 4545555))
+solarSystem.planets.push(new Planet("jupiter", 88484848484))
+
+console.log(solarSystem)
+
+console.log(moment().format('h:mm:ss'))
